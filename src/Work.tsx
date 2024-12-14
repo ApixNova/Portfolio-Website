58f00ea9f0f93ee82a703cd4896194ad1b996973
Apix Nova <laangryanas@gmail.com>
2024-12-14 23:14:15 +0100

    feat: added a new section for Talos

--- src/Work.tsx
+++ src/Work.tsx
@@ -1,10 +1,41 @@
 import cosmo from "./assets/Cosmo.png";
 import musicApp from "./assets/MusicApp.png";
 import starsAbove from "./assets/StarsAbove.png";
+import Talos from "./assets/talos.png";
 
 export default function Work() {
   return (
     <>
+      <div className="work-flex">
+        <div>
+          <h2>Talos</h2>
+          <p>
+            Talos is a mood tracker and a diary app. It is available as a
+            website and an android app built using React Native
+          </p>
+          <p>
+            The app is offline-first, allowing optional use of the Supabase
+            cloud database. It features a sync algorithm to handle the data of
+            logged-in users across their devices
+          </p>
+          <ul>
+            <li>
+              <b>Main Framework:</b> React Native with Expo
+            </li>
+            <li>
+              <b>Data storage:</b> Redux and WatermelonDB for local data,
+              Supabase for online data
+            </li>
+            <li>
+              <b>Other:</b> PostgreSQL, Git, Github and TypeScript
+            </li>
+          </ul>
+        </div>
+        <a href="https://talostheapp.com/" target="_blank">
+          <img className="work-image" src={Talos}></img>
+        </a>
+      </div>
+      <div className="division"></div>
       <div className="work-flex">
         <div>
           <h2>Cosmo</h2>
